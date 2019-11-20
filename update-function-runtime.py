31a8e7c951956bd6fbbe3f948f83f1b38d9120b4
unknown <reza.pahlevi@xtremax.com>
2019-11-20 16:58:20 +0700

    add session profile

--- update-function-runtime.py
+++ update-function-runtime.py
@@ -4,38 +4,47 @@ purpose: for update lambda function runtime
 '''
 
 import boto3
-from pprint import pprint as pp 
+from boto3.session import Session 
+from pprint import pprint as pp
 
-lbd = boto3.client('lambda', region_name='ap-southeast-1')
+class Lambda:
 
-def list_functions():
-    kwargs = {}
-    while True:
-        response = lbd.list_functions(
-            FunctionVersion='ALL',
-            MaxItems=50,
-            **kwargs
+    __api = None 
+    __region = 'ap-southeast-1'
+
+    def __init__(self, session=None):
+        self.__api = boto3.client('lambda', region_name=self.__region) if session is None else session.client('lambda', region_name=self.__region)
+        
+
+    def list_functions(self):
+        kwargs = {}
+        while True:
+            response = self.__api.list_functions(
+                FunctionVersion='ALL',
+                MaxItems=50,
+                **kwargs
+            )
+
+            yield response['Functions']
+
+            if 'NextMarker' in response:
+                kwargs['Marker'] = response['NextMarker']
+            else:
+                break
+
+    def update_function_config(self, function_name):
+        response = self.__api.update_function_configuration(
+            FunctionName=function_name,
+            Runtime='nodejs12.x'
         )
-
-        yield response['Functions']
-
-        if 'NextMarker' in response:
-            kwargs['Marker'] = response['NextMarker']
-        else:
-            break
-
-def update_function_config(function_name):
-    response = lbd.update_function_configuration(
-        FunctionName=function_name,
-        Runtime='nodejs12.x'
-    )
-    print(response)
+        print(response)
 
 if __name__ == '__main__':
-
-    lists_functions = list(list_functions())
+    session = Session(profile_name='<profile-name-here>')
+    lbd = Lambda(session)
+    lists_functions = list(lbd.list_functions())
 
     for list_fns in lists_functions:
         for fn in list_fns:
             if 'nodejs' in fn['Runtime'] and 'LATEST' in fn['Version']:
-                update_function_config(fn['FunctionName'])
+                lbd.update_function_config(fn['FunctionName'])
